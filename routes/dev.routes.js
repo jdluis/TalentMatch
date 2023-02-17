816a7e97e9492d8da0d2ef9f13f3f049943a0580
Alberto Pérez <apleon89@gmail.com>
2023-02-17 14:43:28 +0000

    nav bar layout

--- routes/dev.routes.js
+++ routes/dev.routes.js
@@ -2,6 +2,73 @@
 const express = require('express');
 const router = express.Router();
 
+const Company = require('../models/Company.model.js');
+const Dev = require('../models/Dev.model.js');
 
+router.get('/', async (req, res, next) => {
+    
+    try {
+        const allCompanies = await Company.find()
+        res.render('dev/main.hbs', {
+            allCompanies
+        })
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    
+    try {
+        
+    } catch (error) {
+        next(error)
+    }    
+});
+
+router.get('/:companyId/details', async (req, res, next) => {
+    
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.post('/:companyId/details', async (req, res, next) => {
+    
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get('/profile', async (req, res, next) => {
+    
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get('/profile/edit', async (req, res, next) => {
+    
+    try {
+        
+    } catch (error) {
+        next(error)
+    }    
+});
+
+router.post('/profile/edit', async (req, res, next) => {
+    
+    try {
+        
+    } catch (error) {
+        next(error)
+    }    
+});
 
 module.exports = router;
