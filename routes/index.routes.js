0053b07535fcc3de8e40de143ef09a767fc2b426
Alberto Pérez <apleon89@gmail.com>
2023-02-17 14:11:32 +0000

    dev & company routes created

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -10,4 +10,10 @@ router.get("/", (req, res, next) => {
 const authRoutes = require('./auth.routes.js');
 router.use('/auth', authRoutes);
 
+// Dev Routes
+router.use('/dev', require('./dev.routes.js'));
+
+// Company Routes
+router.use('/company', require('./company.routes.js'));
+
 module.exports = router;
