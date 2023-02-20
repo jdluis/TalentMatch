62127b6baf1a324d4280c5aa77f8820c35463fe3
Alberto Pérez <apleon89@gmail.com>
2023-02-20 13:53:08 +0000

    mvp-014: Seeds DB

---
+++ seeds/devsComp.seed.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+require('../db/index.js');
+
+
+const { default: mongoose } = require('mongoose');
+const Dev = require('../models/Dev.model.js');
+const Company = require('../models/Company.model.js');
+
+const devs = require('./devs.json');
+const companies = require('./companies.json');
+
+const insertData = async () => {
+    try {
+        await Dev.create(devs)
+        await Company.create(companies)
+        console.log('Data added!')
+        mongoose.connection.close()
+    } catch (error) {
+        console.log('Ops! There is a problem!')
+    }
+}
+
+insertData();
