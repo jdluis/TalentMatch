05995b7e21472d70cbfead6a4c248b227d0438c0
jdluis <jdluisdev@gmail.com>
2023-02-21 17:14:11 +0000

    Bonus-003: refac name of user to devUser

--- routes/dev.routes.js
+++ routes/dev.routes.js
@@ -5,7 +5,6 @@ const fileUploader = require('../config/cloudinary.config.js');
 
 const Company = require('../models/Company.model.js');
 const Dev = require('../models/Dev.model.js');
-const Message = require('../models/Message.model.js');
 
 const { isLogged, isDev } = require('../middlewares/auth.middlewares.js');
 
@@ -14,11 +13,11 @@ router.get('/',  async (req, res, next) => {
     try {
         const { search, favComp } = req.query;
 
-        const dev = await Dev.findById(req.session.User._id)
+        const devUser = await Dev.findById(req.session.User._id)
         if ( search || favComp) {
             if (favComp === 'true') {
                 const companiesList = await Company.find({
-                    '_id': {$in: dev.favouritesCompanies},
+                    '_id': {$in: devUser.favouritesCompanies},
                     $or: [
                         {companyName: new RegExp(search, 'i')},
                         {direction: new RegExp(search, 'i')},
@@ -59,10 +58,10 @@ router.get('/:companyId/details', async (req, res, next) => {
 
     try {
         const company = await Company.findById(req.params.companyId)
-        const user = await Dev.findById(req.session.User._id).populate('favouritesCompanies')
+        const devUser = await Dev.findById(req.session.User._id).populate('favouritesCompanies')
         
         let isFavorite = false;
-        user.favouritesCompanies.forEach(eachFav => {
+        devUser.favouritesCompanies.forEach(eachFav => {
 
             if (eachFav.companyName === company.companyName ) {
                 isFavorite = true
@@ -105,10 +104,9 @@ router.post('/:companyId/details', async (req, res, next) => {
 router.get('/profile', async (req, res, next) => {
     
     try {
-        const user = await Dev.findById(req.session.User._id).populate('favouritesCompanies')
-        console.log(user)
+        const devUser = await Dev.findById(req.session.User._id).populate('favouritesCompanies')
         res.render('dev/profile.hbs',{
-            user
+            devUser
         })
     } catch (error) {
         next(error)
@@ -118,22 +116,22 @@ router.get('/profile', async (req, res, next) => {
 router.get('/profile/edit', async (req, res, next) => {
     
     try {
-        const user = await Dev.findById(req.session.User._id);
-        const enumValues = user.schema.path("techSkills").caster.enumValues;
+        const devUser = await Dev.findById(req.session.User._id);
+        const enumValues = devUser.schema.path("techSkills").caster.enumValues;
 
         const selectedTechSkills = [];
         const deselectedTechSkills = [];
 
         enumValues.forEach(eachValue => {
 
-            if (user.techSkills.includes(eachValue)) {
+            if (devUser.techSkills.includes(eachValue)) {
                 selectedTechSkills.push(eachValue)
             } else {
                 deselectedTechSkills.push(eachValue)
             }
         });
         res.render('dev/edit.hbs',{
-            user,
+            devUser,
             selectedTechSkills,
             deselectedTechSkills
         })
@@ -201,9 +199,9 @@ router.post('/profile/edit', fileUploader.single('img'), async (req, res, next) 
 });
 
 router.get('/profile/delete', async (req, res, next) => {
-    const user = await Dev.findById(req.session.User._id).populate('favouritesCompanies')
+    const devUser = await Dev.findById(req.session.User._id).populate('favouritesCompanies')
     res.render('dev/delete.hbs', {
-        user
+        devUser
     })
 });
 
