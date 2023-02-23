045989c5252474f4d47ab1728b1706ea4f026ef2
jdluis <jdluisdev@gmail.com>
2023-02-23 22:09:50 +0000

    title of app changed

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ require("./config")(app);
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
-const projectName = "TalentMatch";
+const projectName = "Talent Match";
 
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalize(projectName)} by Alberto & J.Daniel`;
+
+app.locals.des
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
